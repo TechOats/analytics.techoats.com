f2579ce65564358699f8cf8b6769ffffa537d990
Jon <jon@snowulf.com>
2015-07-10 14:40:05 -0700

    Rip and replace for conversion to techoats

--- js/index.js
+++ js/index.js
@@ -800,10 +800,6 @@
   // plain text for IE
   if (window._ie) {
     console.log("Hi! Please poke around to your heart's content.");
-    console.log("");
-    console.log("If you find a bug or something, please report it at https://github.com/GSA/analytics.usa.gov/issues");
-    console.log("Like it, but want a different front-end? The data reporting is its own tool: https://github.com/18f/analytics-reporter");
-    console.log("This is an open source, public domain project, and your contributions are very welcome.");
   }
 
   // otherwise, let's get fancy
@@ -814,11 +810,8 @@
       medium_bold: "font-size: 10pt; font-weight: bold",
       medium_link: "font-size: 10pt; font-weight: bold; color: #18f",
     };
+
     console.log("%cHi! Please poke around to your heart's content.", styles.big);
-    console.log(" ");
-    console.log("%cIf you find a bug or something, please report it over at %chttps://github.com/GSA/analytics.usa.gov/issues", styles.medium, styles.medium_link);
-    console.log("%cLike it, but want a different front-end? The data reporting is its own tool: %chttps://github.com/18f/analytics-reporter", styles.medium, styles.medium_link);
-    console.log("%cThis is an open source, public domain project, and your contributions are very welcome.", styles.medium);
 
   }
 
